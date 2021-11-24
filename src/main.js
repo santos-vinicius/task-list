3332ea9613afa83367a807a91652dc4f51c8647f
Vinicius Santos <santosviniv@gmail.com>
2021-11-24 04:38:07 -0300

    refatoração e novos arquivos
    Criação e organização de um novo index e style base da aplicação.

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { carregaTarefa } from './components/carregaTarefa.js'
-import { handleNovoItem } from './components/criaTarefa.js'
-
-const novaTarefa = document.querySelector('[data-form-button]')
-
-novaTarefa.addEventListener('click', handleNovoItem)
-
-carregaTarefa()
